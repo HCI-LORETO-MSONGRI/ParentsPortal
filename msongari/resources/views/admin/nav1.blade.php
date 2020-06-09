12e7b36ca16644b62f89aaf44cce4864f3371c62
lynseybwisa <lynsey.bwisa@strathmore.edu>
2020-06-09 23:24:12 +0300

    Admin directory created

---
+++ msongari/resources/views/admin/nav1.blade.php
@@ -0,0 +1,9 @@
+<center>
+		<img src="lcmlogo.jpg" class="profile_image" alt="">
+		<h4>Welcome Admin</h4>
+	</center>
+	<a href="home"><i class="fas fa-home"><span>Home</span></i></a>
+	<a href="register"><i class="fas fa-user-plus"><span>Register Student</span></i></a>
+	<a href="calendar"><i class="fas fa-calendar-times"><span>Update Calendar</span></i></a>
+	<a href="gallery"><i class="fas fa-photo-video"><span>Update Photo Gallery</span></i></a>
+	<a href="feeStructure"><i class="fas fa-file-pdf"><span>Update Fee Structure</span></i></a>
