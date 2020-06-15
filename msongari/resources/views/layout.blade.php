e824b266c8154bcf87d42304948dd570898b6ee8
lynseybwisa <lynsey.bwisa@strathmore.edu>
2020-06-16 02:12:11 +0300

    modified

--- msongari/resources/views/layout.blade.php
+++ msongari/resources/views/layout.blade.php
@@ -6,7 +6,7 @@
 
 	<title>@yield('title', 'LCM Parents Portal')</title>
 	
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="style1.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" >
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
