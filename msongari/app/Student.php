15764e9fd249db647210f232b9d1ff980cb88e5c
lynseybwisa <lynsey.bwisa@strathmore.edu>
2020-06-16 01:53:48 +0300

    Student model modified

--- msongari/app/Student.php
+++ msongari/app/Student.php
@@ -2,9 +2,30 @@
 
 namespace App;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    
+    protected $guarded = [];
+
+    public function getSchoolAttribute($attribute)
+    {
+    	return [
+         1 => 'Boarding',
+         0 => 'Day School',
+    	][$attribute];
+    }
+
+    public function scopeBoarding($query)
+    {
+    	return $query->where('school', 1);
+    }
+
+    public function scopeDay($query)
+    {
+    	return $query->where('school', 0);
+    }
+
+   
 }
